dbab188d7b89ac26d8d1097fe9c72aa5b25237f9
Rahul Kumar <rrahul772k@gmail.com>
2023-03-02 15:09:37 +0530

    updated code

---
+++ dashboard/urls.py
@@ -0,0 +1,28 @@
+from django.urls import path
+from .import views  
+
+urlpatterns = [
+    path('',views.home, name="home"),
+
+    path('notes/',views.notes, name="notes"),
+    path('delete_note/<int:pk>/',views.delete_note, name="delete_note"),
+    path('notes_detail/<int:pk>/',views.NotesDetailView.as_view(), name="notes_detail"),
+
+    path('homework/',views.homework, name="homework"),
+    path('update_homework/<int:pk>/',views.update_homework, name="update_homework"),
+    path('delete_homework/<int:pk>/',views.delete_homework, name="delete_homework"),
+
+    path('youtube/',views.youtube, name="youtube"),
+
+    path('todo/',views.todo, name="todo"),
+    path('update_todo/<int:pk>/',views.update_todo, name="update_todo"),
+    path('delete_todo/<int:pk>/',views.delete_todo, name="delete_todo"),
+
+    path('books/',views.books, name="books"),
+
+    path('dictionary/',views.dictionary, name="dictionary"),
+
+    path('wiki/',views.wiki, name="wiki"),
+
+    path('conversion/',views.conversion, name="conversion"),
+]
